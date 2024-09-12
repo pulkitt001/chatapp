5e1f591944d6112259fd037768bfeccc1471efa1
pulkitt001 <pulkitpal0001@gmail.com>
2024-09-13 03:22:35 +0530

    fix the socket

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+
 import authroute from "./routes/authroutes.js";
 import messageroutes from "./routes/messageroutes.js";
 import userroutes from "./routes/userroutes.js";
 import cors from 'cors'
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import {app,server} from "./socket/socket.js";
 
 dotenv.config();
-const app = express();
+// const app = express();
 const PORT =  process.env.PORT||5000
 
 const corsOptions = {
@@ -34,25 +36,9 @@ app.use("/api/user",userroutes);
 // });
 
 
-app.listen(PORT,() => {
+server.listen(PORT,() => {
     connectToMongoDB();
     console.log(`Server Running on port ${PORT}`)
 });
 
 
-
-
-
-
-
-// // app.get("/api/auth/signup",(req,res)=>{
-// //     console.log("signup route");
-// // });
-
-// // app.get("/api/auth/login",(req,res)=>{
-// //     console.log("login route");
-// // });
-
-// // app.get("/api/auth/logout",(req,res)=>{
-// //     console.log("logout route");
-// // });
